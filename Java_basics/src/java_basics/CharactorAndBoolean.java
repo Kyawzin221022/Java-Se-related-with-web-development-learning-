d0d870fa156335a28af5088a40e3cf49d1791fac
Kyaw Zin Khant <chityinoo0@gmail.com>
2023-05-18 17:20:36 +0630

    Just started Java course

---
+++ Java_basics/src/java_basics/CharactorAndBoolean.java
@@ -0,0 +1,30 @@
+package java_basics;
+
+public class CharactorAndBoolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char bigO = 'O';
+		char smallR = 'r';
+		char nine = '9';
+		char symbol = '&';
+		char unicode = '\u0534';
+		
+		System.out.println(bigO);
+		System.out.println(smallR);
+		System.out.println(nine);
+		System.out.println(symbol);
+		System.out.println(unicode);
+	
+		
+		boolean flag1 = true;
+		boolean flag2 = false;
+		
+		System.out.println(flag1);
+		System.out.println(flag2);
+		
+
+	}
+
+}
---
+++ Java_basics/src/java_basics/FloatingPointTest.java
@@ -0,0 +1,22 @@
+package java_basics;
+
+public class FloatingPointTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float floatValue = 23.456789099399332f;
+		float floatMax = Float.MAX_VALUE;
+		float floatMin = Float.MIN_NORMAL;
+		float floatPower = 1.2e7f;
+		
+		System.out.println(floatValue);
+		System.out.println(floatMax);
+		System.out.println(floatMin);
+		System.out.println(floatPower);
+		
+		double doubleValue = 23.326888787979999999999999d;
+		System.out.println(doubleValue);
+	}
+
+}
