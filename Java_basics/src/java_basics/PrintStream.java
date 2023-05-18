8d48e80cabb21435e6ebbf03aee00d630ed05001
Kyaw Zin Khant <chityinoo0@gmail.com>
2023-05-18 16:40:02 +0630

    Just started my java course

---
+++ Java_basics/src/java_basics/PrintStream.java
@@ -0,0 +1,18 @@
+package java_basics;
+
+public class PrintStream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		//print
+		
+		System.out.println("Java");
+		System.out.print("Geek");
+		
+		System.out.printf(" Hello World %d\n",5);
+	
+		System.out.printf("%d %s %.3f",1,"Mg Mg",34.56789);
+	}
+
+}
